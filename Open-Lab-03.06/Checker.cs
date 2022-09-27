8966b51f583975c8a5da3b33d385d6d214443674
einnarth <113421210+einnarth@users.noreply.github.com>
2022-09-27 23:11:25 +0200

    Update Checker.cs

--- Open-Lab-03.06/Checker.cs
+++ Open-Lab-03.06/Checker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._06
 {
@@ -6,7 +6,7 @@ namespace Open_Lab_03._06
     {
         public bool HasSpaces(string str)
         {
-            throw new NotImplementedException();
+            return str.Contains(" ");
         }
     }
 }
